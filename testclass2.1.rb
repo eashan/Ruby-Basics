281d6a0d73fb8fcd7eda0d0b7f7ce0d1491c51f1
skytap <skytap@host-1.skytap.example>
2016-10-26 01:34:07 -0700

    Updated Accessors for Initialized variables

---
+++ testclass2.1.rb
@@ -0,0 +1,35 @@
+class Thing
+	attr_reader :description
+	attr_writer :description
+	attr_writer :name
+
+	def initialize(aName, aDescription)
+		@name = aName
+		@description = aDescription
+	end
+	
+	#get Accessor for @name(Like from previous example)
+	def name
+		return @name.capitalize
+	end
+
+
+end
+
+class Treasure < Thing
+	attr_accessor :value
+	def initialize(aName, aDescription)
+		super(aName, aDescription)
+	end
+end
+
+wand = Treasure.new("Elder Wand", "One wand to rule them all")
+wand.value = 1000000
+puts "Name: #{wand.name}, description: #{wand.description}, value: #{wand.value}"
+
+wand.value = 11
+wand.description << ", now favors harry "
+
+puts "Name: #{wand.name}, description: #{wand.description}, value: #{wand.value}"
+
+
